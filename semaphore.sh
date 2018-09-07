b9fc4fe2001742ea79a8c5adecab4be5c1339d58
Lorenz Bauer <lmb@cloudflare.com>
2018-09-07 11:22:01 +0100

    Cache rkt on CI
    Running a build is currently slow, since we have to pull images every time.
    It's not straightforward to do cache images, but we can avoid downloading rkt
    itself.

--- semaphore.sh
+++ semaphore.sh
@@ -27,26 +27,24 @@ readonly kernel_versions=("4.16.9")
 # The rkt version which is set as a dependency for
 # the custom stage1-kvm images
 readonly rkt_version="1.30.0"
-
-readonly pkg_name="github.com/newtools/ebpf"
+readonly rkt_dir="${SEMAPHORE_CACHE_DIR}/rkt-${rkt_version}"
+readonly rkt="${rkt_dir}/rkt"
 
 # Download rkt if not available yet as Semaphore CI
 # doesn't have rkt at the time of writing
-if [[ ! -f "./rkt/rkt" ]] \
-  || [[ ! "$(./rkt/rkt version | awk '/rkt Version/{print $3}')" == "${rkt_version}" ]]; then
-
+if [[ ! -f "${rkt}" ]]; then
   curl -LsS "https://github.com/coreos/rkt/releases/download/v${rkt_version}/rkt-v${rkt_version}.tar.gz" \
-    -o rkt.tgz
+    -o "${SEMAPHORE_CACHE_DIR}/rkt-${rkt_version}.tgz"
 
-  mkdir -p rkt
-  tar -xf rkt.tgz -C rkt --strip-components=1
+  mkdir -p "${rkt_dir}"
+  tar -xf "${SEMAPHORE_CACHE_DIR}/rkt-${rkt_version}.tgz" -C "${rkt_dir}" --strip-components=1
 fi
 
 curl -s https://codecov.io/bash > codecov.sh
 chmod +x codecov.sh
 # Pre-fetch stage1 dependency due to rkt#2241
 # https://github.com/coreos/rkt/issues/2241
-sudo ./rkt/rkt image fetch --insecure-options=image "coreos.com/rkt/stage1-kvm:${rkt_version}" >/dev/null
+sudo "${rkt}" image fetch --insecure-options=image "coreos.com/rkt/stage1-kvm:${rkt_version}" >/dev/null
 
 for kernel_version in "${kernel_versions[@]}"; do
   # The stage1-kvm image to use for the tests
@@ -66,30 +64,27 @@ for kernel_version in "${kernel_versions[@]}"; do
 
   # timeout can be used to make sure tests finish in
   # a reasonable amount of time
-  sudo timeout --foreground --kill-after=10 5m \
-    ./rkt/rkt \
+  sudo timeout --foreground --kill-after=10 2m \
+    "${rkt}" \
     run --interactive \
     --uuid-file-save=./rkt-uuid \
     --insecure-options=image,all-run \
     --dns=8.8.8.8 \
     --stage1-name="${stage1_name}" \
     --volume=src,kind=host,source="${SEMAPHORE_PROJECT_DIR}" \
-    --volume=go,kind=host,source="/home/runner/workspace" \
-    --mount=volume=src,target="${SEMAPHORE_PROJECT_DIR}" \
-    --mount=volume=go,target="/go" \
+    --mount=volume=src,target="/src/${SEMAPHORE_PROJECT_NAME}" \
     docker://golang:1.10-alpine \
-    --environment=GOPATH=/go \
     --exec=/bin/sh -- -c \
     "mount -t tmpfs tmpfs /tmp &&
       mount -t bpf bpf /sys/fs/bpf &&
       mount -t debugfs debugfs /sys/kernel/debug/ &&
-      cd /go/src/${pkg_name} &&
-      go go build -v ./... &&
+      cd /src/${SEMAPHORE_PROJECT_NAME} &&
+      go build -v ./... &&
       go test -coverprofile=coverage.txt -covermode=atomic -v ./..."
 
   # Determine exit code from pod status due to rkt#2777
   # https://github.com/coreos/rkt/issues/2777
-  test_status=$(sudo ./rkt/rkt status "$(<rkt-uuid)" | awk '/app-/{split($0,a,"=")} END{print a[2]}')
+  test_status=$(sudo "${rkt}" status "$(<rkt-uuid)" | awk '/app-/{split($0,a,"=")} END{print a[2]}')
   if [[ $test_status -ne 0 ]]; then
     exit "$test_status"
   fi
